4a5dd0e7909650e87b370c0e37d02b787c2cf28a
Alyamr96 <aliamrosman16@gmail.com>
2021-08-31 17:07:59 +0200

    implemented a delete multiple feature and Renaming Files

--- MyDrive/Controllers/FolderController.cs
+++ MyDrive/Controllers/FolderController.cs
@@ -18,6 +18,8 @@ namespace MyDrive.Controllers
     {
         static string absoloutePath = @"C: \Users\cashless\Desktop\MyDrive\MyDrive\Files";
         static string folderPathWhenRename = "";
+        static List<string> FoldersToDelete = new List<string>();
+        static List<string> FilesToDelete = new List<string>();
         /*private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
 
@@ -401,6 +403,10 @@ namespace MyDrive.Controllers
         {
             string path = folderPathWhenRename;
             folderPathWhenRename = "";
+            path = path.Replace("'", ".");
+            string[] extentionArray = path.Split('.');
+            string extention = extentionArray[extentionArray.Length - 1];
+            string NewFileName = folder.Name + '.' + extention;
             string returnPath = "";
             string[] words = path.Split(';');
             string oldFolderName = words[words.Length - 1];
@@ -416,7 +422,7 @@ namespace MyDrive.Controllers
             oldPath = oldPath.Replace("/", @"\");
             oldPath = absoloutePath + @"\" + oldPath;
             FileInfo di = new FileInfo(oldPath);
-            di.MoveTo(Path.Combine(di.DirectoryName, folder.Name));
+            di.MoveTo(Path.Combine(di.Directory.FullName, NewFileName));
 
             if (returnPath != "")
             {
@@ -424,7 +430,162 @@ namespace MyDrive.Controllers
             }
             else
                 return RedirectToAction("GetFolders1");
+            //return Content(oldPath +"+" + NewFileName + "+" + extention);
 
+        }
+
+        [Route("Folder/DeleteMultiple/{folderName}")]
+        public ActionResult DeleteMultiple(string folderName)
+        {
+            string myFolderName = folderName.Replace(";", @"\");
+            string path = absoloutePath + @"/" + myFolderName;
+            List<string> dirs = new List<string>(Directory.EnumerateDirectories(path));
+            List<string> foldersPathAfterFile = new List<string>(dirs.Count);
+            List<string> folderPathAfterFileWithoutBackSlash = new List<string>(dirs.Count);
+            List<string> folderNames = new List<string>(dirs.Count);
+            List<Folder> folders = new List<Folder>(dirs.Count);
+            foreach (var directory in dirs)
+            {
+                string fullpath = Path.GetFullPath(directory).TrimEnd(Path.DirectorySeparatorChar);
+                string projectName = directory.Split(Path.DirectorySeparatorChar).Last();
+                folderNames.Add(projectName);
+            }
+            foreach (var directory in dirs)
+            {
+                for (int i = 0; i < directory.Length; i++)
+                {
+                    string test = directory.Substring(i, 5);
+                    if (test.CompareTo("Files") == 0)
+                    {
+                        int number = i + 6;
+                        foldersPathAfterFile.Add(directory.Substring(number));
+                        break;
+                    }
+                }
+            }
+            foreach (var directory in foldersPathAfterFile)
+            {
+                string testing = directory.Replace(@"\", ";");
+                folderPathAfterFileWithoutBackSlash.Add(testing);
+            }
+            for (int i = 0; i < dirs.Count; i++)
+            {
+                folders.Add(new Folder { Name = folderNames[i], Path = folderPathAfterFileWithoutBackSlash[i] });
+            }
+            // Starting here we're getting the files
+            string[] fileEntries = Directory.GetFiles(path);
+            List<string> filesPathAfterFile = new List<string>(fileEntries.Length);
+            string[] fileNames = new string[fileEntries.Length];
+            List<FileModel> files = new List<FileModel>(fileEntries.Length);
+            for (int i = 0; i < fileEntries.Length; i++)
+            {
+                string fullpath = Path.GetFullPath(fileEntries[i]).TrimEnd(Path.DirectorySeparatorChar);
+                string projectName = fileEntries[i].Split(Path.DirectorySeparatorChar).Last();
+                fileNames[i] = projectName;
+            }
+            for (int j = 0; j < fileEntries.Length; j++)
+            {
+                for (int i = 0; i < fileEntries[j].Length; i++)
+                {
+                    string test = fileEntries[j].Substring(i, 5);
+                    if (test.CompareTo("Files") == 0)
+                    {
+                        int number = i + 6;
+                        filesPathAfterFile.Add(fileEntries[j].Substring(number));
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < fileEntries.Length; i++)
+            {
+                files.Add(new FileModel { Name = fileNames[i], Path = filesPathAfterFile[i] });
+            }
+            // FoldersandFilesViewModel
+            var FoldersAndFiles = new FoldersandFilesViewModel
+            {
+                Folders = folders,
+                Files = files
+            };
+            return View(FoldersAndFiles);
+        }
+
+        [Route("Folder/StoreFolderInList/{folderPath}")]
+        public ActionResult storeDeletedFoldersInArray(string folderPath)
+        {
+            string myFolderPath = folderPath.Replace(";", @"\");
+            string path = absoloutePath + @"/" + myFolderPath;
+            FoldersToDelete.Add(path);
+            string[] words = folderPath.Split(';');
+            string returnPath = "";
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                returnPath = returnPath + words[i] + ";";
+            }
+            return RedirectToAction("DeleteMultiple/" + returnPath);
+        }
+
+        [Route("Folder/StoreFileInList/{filePath}")]
+        public ActionResult storeDeletedFilesInArray(string filePath)
+        {
+            string myFilePath = filePath.Replace(";", @"\");
+            string myFinalPath = myFilePath.Replace("'", ".");
+            string path = absoloutePath + @"/" + myFinalPath;
+            FilesToDelete.Add(path);
+            string[] words = filePath.Split(';');
+            string returnPath = "";
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                returnPath = returnPath + words[i] + ";";
+            }
+            return RedirectToAction("DeleteMultiple/" + returnPath);
+        }
+
+        public ActionResult DeleteAllSelected()
+        {
+            List<string> FoldersToDeleteNoDuplicates = FoldersToDelete.Distinct().ToList();
+            List<string> FilesToDeleteNoDuplicates = FilesToDelete.Distinct().ToList();
+            //FoldersToDelete.Clear();
+            //FilesToDelete.Clear();
+            ViewBag.Folders = FoldersToDeleteNoDuplicates;
+            ViewBag.Files = FilesToDeleteNoDuplicates;
+            return View();
+        }
+
+        public ActionResult DeleteAllSelectedFoldersAndFiles()
+        {
+            List<string> FoldersToDeleteNoDuplicates = FoldersToDelete.Distinct().ToList();
+            List<string> FilesToDeleteNoDuplicates = FilesToDelete.Distinct().ToList();
+            for(int i=0; i<FoldersToDeleteNoDuplicates.Count; i++)
+            {
+                Directory.Delete(FoldersToDeleteNoDuplicates[i], true);
+            }
+            for (int i = 0; i < FilesToDeleteNoDuplicates.Count; i++)
+            {
+                System.IO.File.Delete(FilesToDeleteNoDuplicates[i]);
+            }
+            FoldersToDelete.Clear();
+            FilesToDelete.Clear();
+            // starting here are operations to return to folders
+            string returnPath = FoldersToDeleteNoDuplicates[0];
+            for(int i=0; i<returnPath.Length; i++)
+            {
+                string test = returnPath.Substring(i, 5);
+                if (test.CompareTo("Files") == 0)
+                {
+                    int number = i + 6;
+                    returnPath = returnPath.Substring(number);
+                    break;
+                }
+            }
+            returnPath = returnPath.Replace(@"\", ";");
+            string[] words = returnPath.Split(';');
+            string returnPath1 = "";
+            for (int i = 0; i < words.Length - 1; i++)
+            {
+                returnPath1 = returnPath1 + words[i] + ";";
+            }
+            returnPath1 = returnPath1.Substring(0, returnPath1.Length - 1);
+            return RedirectToAction("GetFolderFromPath/" + returnPath1);
         }
     }
 }
