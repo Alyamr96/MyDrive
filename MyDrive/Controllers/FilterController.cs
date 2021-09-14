83a97e15acf14b190e820581f9f49a3ba9772735
Alyamr96 <aliamrosman16@gmail.com>
2021-09-14 16:15:25 +0200

    added filters

---
+++ MyDrive/Controllers/FilterController.cs
@@ -0,0 +1,35 @@
+﻿using MyDrive.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyDrive.Controllers
+{
+    public class FilterController : Controller
+    {
+        // GET: Filter
+        public ActionResult FilterOptions()
+        {
+            List<string> filters = new List<string> { "User Name", "Date of Upload", "Size", "Folder/File Name", "Company"};
+            var viewModel = new FiltersAppliedViewModel { FiltersApplied = filters};
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult FilterSelected(FiltersAppliedViewModel model)
+        {
+            if (model.FilterSelected == "Date of Upload")
+                return View("DateFilterView");
+            else if (model.FilterSelected == "User Name")
+                return View("UserNameFilter");
+            else if (model.FilterSelected == "Size")
+                return View("SizeFilter");
+            else if (model.FilterSelected == "Folder/File Name")
+                return View("FolderFileNameFilter");
+            else
+                return View("CompanyFilter");
+        }
+    }
+}
---
+++ MyDrive/ViewModels/FiltersAppliedViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyDrive.ViewModels
+{
+    public class FiltersAppliedViewModel
+    {
+        public List<string> FiltersApplied { get; set; }
+
+        public string FilterSelected { get; set; }
+    }
+}
